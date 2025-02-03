e6158d80a7ee3f2ebf69510cc0a26d56f6fe7a47
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-02-02 23:56:13 -0800

    preprocessor: startup signal

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -282,6 +282,8 @@ def listen_to_sqs():
 
 
 def run():
+    sns.send("Preprocessor", "Preprocessor started/restarted")
+    logger.info("Preprocessor started/restarted")
     if GeneralKeys.DEPLOYMENT == "dev":
         # process_and_upload_dataset(
         #     "file:///mnt/d/datasets/VisDroneSmall.zip",
