d95dbb038da61688f27d3e4e4563745284b1f7ab
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-04-30 14:29:01 -0700

    major refactor

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -7,19 +7,22 @@ import time
 import traceback
 import zipfile
 from pathlib import Path
+from typing import Optional, Tuple
 
 import boto3
 import wget
-import yaml
 
 from aws_handler import S3Handler, SNSHandler
 from db import db_manager
+from db.models import Dataset
 from db.queries import queries
 from db.writer import DatabaseWriter
 from keys import DatasetKeys, GeneralKeys, PreProcessorKeys, TrainerKeys
 from preprocessor.dataset import (
-    convert_mask_to_bbox,
+    convert_roboflow_yolo_to_bbox,
     download_dataset_from_roboflow,
+    get_roboflow_classnames_and_fix,
+    perform_combine,
     visdrone2yolo,
     yolo_to_coco,
 )
@@ -31,43 +34,122 @@ sns = SNSHandler(logger=logger)
 s3 = S3Handler(bucket=GeneralKeys.S3_BUCKET_NAME, logger=logger)
 
 
-def process_and_upload_dataset(data):
+def verify_dataset_info_for_processing(data) -> Tuple[bool, str]:
     dataset_name = data.get("name", None)
     url = data.get("url", None)
     dtype = data.get("datasetType", None)
     if dataset_name is None or url is None or dtype is None:
-        logger.warning(f"Something went wrong trying to process {json.dumps(data)}")
-        sns.send(
-            "Something went wrong",
+        return (
+            False,
             f"Something went wrong trying process_and_upload_dataset with the following:\n\n{json.dumps(data)}",
         )
-        return
 
     should_combine = data.get("shouldCombine", False)
     combine_id = data.get("combineID", None)
     if should_combine and combine_id is None:
-        logger.warning(f"combine_id is required when should_combine is True {json.dumps(data)}")
-        sns.send(
-            "Something went wrong",
-            f"process_and_upload_dataset tried to combine without combine id:\n\n{json.dumps(data)}",
-        )
-        return
+        return False, f"process_and_upload_dataset tried to combine without combine id:\n\n{json.dumps(data)}"
 
     if dtype not in PreProcessorKeys.SUPPORTED_TYPES:
-        logger.warning(f"{dtype} download type not supported")
-        sns.send(
-            "Something went wrong",
-            f"{dtype} download type not supported",
-        )
+        return False, f"{dtype} download type not supported"
+
+    return True, ""
+
+
+def check_valid_combination_id(combine_id) -> Tuple[bool, str, Optional[Dataset]]:
+    is_valid = True
+    logger.info("Finding dataset to combine with")
+    dataset_obj = queries().get_dataset_by_id(combine_id)
+    dataset_obj_old = None
+    combine_id_old = None
+
+    first_fail_msg = "Could not find provided dataset, cannot combine!"
+    if dataset_obj is None:
+        is_valid = False
+
+    if dataset_obj is not None and dataset_obj.datasetType.value == "coco":
+        # Assumption is that yolo will always be defined before coco
+        # The rest of the code assumes a yolo format will be combined with before continuing
+        logger.info("A coco dataset was provided to combine, attempting to find yolo format")
+        first_fail_msg = "Could not find yolo format of provided dataset, cannot combine!"
+        dataset_obj_old = dataset_obj
+        combine_id_old = combine_id
+        combine_id -= 1
+        dataset_obj = queries().get_dataset_by_id(combine_id)
+        if dataset_obj is None:
+            is_valid = False
+        if dataset_obj is not None and dataset_obj.datasetType.value != "yolo":
+            is_valid = False
+
+    if not is_valid:
+        msg = [
+            first_fail_msg,
+            "Instead, the dataset will be uploaded and converted on its own",
+            f"timestamp: {time.strftime('%Y-%m-%d %H:%M:%S')}",
+            "Tried combining with:",
+        ]
+
+        if dataset_obj_old is not None:
+            msg.append(f"Database entry for ID {combine_id_old}:\n")
+            msg.append(f"{dataset_obj_old}")
+
+        if dataset_obj is not None:
+            msg.append(f"Database entry for ID {combine_id}:\n")
+            msg.append(f"{dataset_obj}")
+
+        msg = "\n".join(msg)
+        return is_valid, msg, None
+    return is_valid, "", dataset_obj
+
+
+def upload_dataset_and_make_entry(name, tags, links, dir_name, checksum_blob, key):
+    current_ts = str(int(time.time()))
+    checksum_fn = f"checksum_{current_ts}.txt"
+    zip_name = f"{key}_{current_ts}.zip"
+
+    with open(checksum_fn, "w") as fd:
+        fd.write("\n".join(checksum_blob))
+
+    session = next(db_manager.get_db())
+    writer = DatabaseWriter(session)
+
+    dataset_type = queries().get_dataset_type_by_value(key)
+    if dataset_type is None:
+        logger.warning(f"{key} dataset type does not exist? Check database")
         return
 
+    ### upload to s3 and make db entry
+    s3.upload_file_to_s3(checksum_fn, "dataset", checksum_fn)
+    s3.upload_zip_to_s3(dir_name, "dataset", zip_name=zip_name)
+    assert isinstance(dataset_type.id, int)
+    writer.create_dataset(
+        datasetTypeId=dataset_type.id,
+        name=name,
+        tags=tags,
+        links=links,
+        s3Key=f"dataset/{zip_name}",
+        checksumBlobS3Key=f"dataset/{checksum_fn}",
+    )
+
+
+def process_and_upload_dataset(data):
+    success, msg = verify_dataset_info_for_processing(data)
+    if not success:
+        logger.warning(msg)
+        sns.send("Something went wrong", msg)
+        return
+
+    dataset_name = data.get("name", None)
+    url = data.get("url", None)
+    dtype = data.get("datasetType", None)
+    should_combine = data.get("shouldCombine", False)
+    combine_id = data.get("combineID", None)
     tags = data.get("tags", [dataset_name.lower().replace(" ", "-").strip()])
 
     sns.send(
-        f"Converting {dtype} dataset | {dataset_name}",
+        f"Converting {dtype} dataset | {dataset_name} | start",
         "\n".join(
             [
-                f"Converting {dtype} dataset",
+                f"Converting {dtype} dataset | Start",
                 f"timestamp: {time.strftime('%Y-%m-%d %H:%M:%S')}",
                 f"tags: {tags}\n\n",
                 "Data dump:",
@@ -79,44 +161,9 @@ def process_and_upload_dataset(data):
     dataset_obj = None
     links = [url]
     if should_combine:
-        logger.info("Finding dataset to combine with")
-        dataset_obj = queries().get_dataset_by_id(combine_id)
-        if dataset_obj is None:
-            msg = f"Dataset at id {combine_id} not found"
-            logger.info(msg)
-            sns.send("Error", msg)
-            return
-        if dataset_obj.datasetType.value == "coco":
-            # Assumption is that yolo will always be defined before coco
-            # The rest of the code assumes a yolo format will be combined with before continuing
-            logger.info("A coco dataset was provided to combine, attempting to find yolo format")
-            combine_id -= 1
-            dataset_obj_old = dataset_obj
-            dataset_obj = queries().get_dataset_by_id(combine_id)
-            if dataset_obj is None:
-                msg = f"Dataset at id {combine_id} not found"
-                logger.info(msg)
-                sns.send("Error", msg)
-                return
-            if dataset_obj.datasetType.value != "yolo":
-                logger.warning("Could not find yolo format for combining, creating new dataset without combination")
-                sns.send(
-                    f"Converting {dtype} dataset | {dataset_name} | IMPORTANT",
-                    "\n".join(
-                        [
-                            "Could not find yolo format of provided dataset, cannot combine!",
-                            "Instead, the dataset will be uploaded and converted on its own",
-                            f"timestamp: {time.strftime('%Y-%m-%d %H:%M:%S')}",
-                            "Tried combining with",
-                            f"Database entry for ID {combine_id}:\n",
-                            f"{dataset_obj_old}",
-                            f"Database entry fix attempt ID {combine_id - 1}:\n",
-                            f"{dataset_obj}",
-                        ]
-                    ),
-                )
-                should_combine = False
-        if should_combine:
+        valid_combination_id, msg, dataset_obj = check_valid_combination_id(combine_id)
+        if valid_combination_id:
+            assert isinstance(dataset_obj, Dataset)
             assert isinstance(dataset_obj.links, list)
             assert isinstance(dataset_obj.tags, list)
             links.extend(dataset_obj.links)
@@ -124,8 +171,13 @@ def process_and_upload_dataset(data):
             tags.extend(dataset_obj.tags)
             tags = list(set(tags))
             s3.download_file(dataset_obj.s3Key, "yolo_old.zip")
+        else:
+            should_combine = False
+            logger.warning(msg)
+            sns.send(f"Converting {dtype} dataset | {dataset_name} | IMPORTANT", msg)
 
     dir_name = None
+    names = None
 
     ### Create YOLO format of datasets
     if dtype == PreProcessorKeys.TYPE_ROBOFLOW:
@@ -133,33 +185,8 @@ def process_and_upload_dataset(data):
         dir_name = download_dataset_from_roboflow(url, PreProcessorKeys.ROBOFLOW_YOLOV11, PreProcessorKeys.ROBOFLOW_KEY)
         dir_name = Path(dir_name)
         logger.info(f"Finished download of {url} to {dir_name}")
-        with open(dir_name / "data.yaml", "r") as fd:
-            yaml_content = yaml.safe_load(fd)
-            names = [name.lower() for name in yaml_content["names"]]
-            yaml_content["names"] = names
-
-        with open(dir_name / "data.yaml", "w") as fd:
-            yaml.safe_dump(yaml_content, fd)
-
-        splits = ["test", "train", "valid"]
-        for split in splits:
-            labels_path = dir_name / split / "labels"
-            for f in labels_path.glob("*.txt"):
-                clean = []
-                with open(f, "r") as fd:
-                    lines = fd.read()
-                    lines = lines.strip()
-                    lines = lines.splitlines()
-                for line in lines:
-                    tmp = line.strip().split()
-                    if len(tmp) > 5:
-                        tmp = convert_mask_to_bbox(tmp)
-                        tmp = [str(t) for t in tmp]
-                    tmp = " ".join(tmp)
-                    clean.append(tmp)
-                clean = "\n".join(clean)
-                with open(f, "w") as fd:
-                    fd.write(clean)
+        names = get_roboflow_classnames_and_fix(dir_name)
+        convert_roboflow_yolo_to_bbox(dir_name)
 
     elif dtype == PreProcessorKeys.TYPE_VISDRONE:
         logger.info(f"Started download for dataset at {url}")
@@ -173,169 +200,37 @@ def process_and_upload_dataset(data):
         names = PreProcessorKeys.VISDRONE_CLASSES
         visdrone2yolo(dir_name, names)
 
-        os.remove("visdrone.zip")
-
-    else:
-        logger.info("Should not have gotten here")
-        return
-
     try:
         assert isinstance(dir_name, Path)
         assert isinstance(names, list)
     except AssertionError:
         logger.error("Something went wrong converting to YOLO")
+        sns.send(
+            f"Converting {dtype} dataset | {dataset_name} | IMPORTANT",
+            "Something went wrong converting to YOLO, process aborted.",
+        )
         return
 
     checksum_blob = []
     if should_combine:
-        try:
-            assert dataset_obj is not None
-        except AssertionError:
-            logger.error("Something went wrong when asserting dataset_obj during combining step")
-            return
-
-        if s3.check_file_exists(dataset_obj.checksumBlobS3Key, logger=logger):
-            s3.download_file(dataset_obj.checksumBlobS3Key, "checksum_blob.txt")
-            with open("checksum_blob.txt", "r") as fd:
-                checksum_blob = fd.read().splitlines()
-            os.remove("checksum_blob.txt")
-        logger.info("Combining datasets")
-
-        with zipfile.ZipFile("yolo_old.zip", "r") as z:
-            z.extractall("old")
-        old_dir = Path("old")
-
-        with open(old_dir / "data.yaml", "r") as fd:
-            old_yaml = yaml.safe_load(fd)
-            old_names = old_yaml["names"]
-        with open(dir_name / "data.yaml", "r") as fd:
-            new_yaml = yaml.safe_load(fd)
-            new_names = new_yaml["names"]
-
-        combined_names = old_names
-        class_id_map = {}
-        for i, name in enumerate(new_names):
-            if name not in old_names:
-                combined_names.append(name)
-                class_id_map[str(i)] = str(len(combined_names) - 1)
-            else:
-                class_id_map[str(i)] = str(old_names.index(name))
-
-        with open(old_dir / "data.yaml", "w") as fd:
-            new_yaml["names"] = combined_names
-            new_yaml["nc"] = len(combined_names)
-            yaml.safe_dump(new_yaml, fd)
-
-        current_ts = int(time.time())
-        skipped = 0
-        for split in ["test", "train", "valid"]:
-            for img_path in (dir_name / split / "images").glob("*"):
-                img_checksum = hashlib.md5(img_path.read_bytes()).hexdigest()
-                if img_checksum in checksum_blob:
-                    skipped += 1
-                    continue
-                checksum_blob.append(img_checksum)
-
-                txt_path = dir_name / split / "labels" / (img_path.stem + ".txt")
-                with open(txt_path, "r") as fd:
-                    lines = fd.read().strip().splitlines()
-                new_lines = []
-                for line in lines:
-                    tmp = line.split()
-                    try:
-                        tmp[0] = class_id_map[tmp[0]]
-                    except KeyError:
-                        tmp[0] = class_id_map[str(int(float(tmp[0])))]
-                    new_lines.append(" ".join(tmp))
-                with open(txt_path, "w") as fd:
-                    fd.write("\n".join(new_lines))
-
-                # ts to avoid name conflicts
-                shutil.move(str(img_path), str(old_dir / split / "images" / f"{current_ts}_{img_path.name}"))
-                shutil.move(str(txt_path), str(old_dir / split / "labels" / f"{current_ts}_{txt_path.name}"))
-
-        shutil.rmtree(dir_name)
-        os.remove("yolo_old.zip")
-        dir_name = old_dir
+        dir_name, checksum_blob, skipped = perform_combine(dataset_obj, dir_name, s3)
         logger.info(f"Finished combining datasets with {skipped} images skipped")
-
     else:
         for split in ["test", "train", "valid"]:
             for img_path in (dir_name / split / "images").glob("*"):
                 img_checksum = hashlib.md5(img_path.read_bytes()).hexdigest()
                 checksum_blob.append(img_checksum)
 
-    current_ts = str(int(time.time()))
-    checksum_fn = f"checksum_{current_ts}.txt"
-    zip_name = f"{DatasetKeys.YOLO_FORMAT}_{current_ts}.zip"
-
-    with open(checksum_fn, "w") as fd:
-        fd.write("\n".join(checksum_blob))
-
-    session = next(db_manager.get_db())
-    writer = DatabaseWriter(session)
-
-    yolo_dataset_type = queries().get_dataset_type_by_value("yolo")
-    if yolo_dataset_type is None:
-        logger.warning("YOLO dataset type does not exist? Check database")
-        return
-
-    ### upload to s3 and make db entry
-    s3.upload_file_to_s3(checksum_fn, "dataset", checksum_fn)
-    s3.upload_zip_to_s3(dir_name, "dataset", zip_name=zip_name)
-    assert isinstance(yolo_dataset_type.id, int)
-    writer.create_dataset(
-        datasetTypeId=yolo_dataset_type.id,
-        name=dataset_name,
-        tags=tags,
-        links=links,
-        s3Key=f"dataset/{zip_name}",
-        checksumBlobS3Key=f"dataset/{checksum_fn}",
-    )
-
-    ### Convert to COCO format
-    splits = ["test", "train", "valid"]
-    logger.info("Converting dataset to COCO format")
-    for split in splits:
-        yolo_to_coco(
-            dir_name / split / "images",
-            dir_name / split / "labels",
-            dir_name / split / "images/_annotations.coco.json",
-            names,
-        )
-        shutil.rmtree(dir_name / split / "labels")
-        for file_path in (dir_name / split / "images").glob("*"):
-            shutil.move(str(file_path), str(dir_name / split))
-        os.rmdir(dir_name / split / "images")
-    os.remove(dir_name / "data.yaml")
-
-    zip_name = f"{DatasetKeys.COCO_FORMAT}_{current_ts}.zip"
-
-    coco_dataset_type = queries().get_dataset_type_by_value("coco")
-    if coco_dataset_type is None:
-        logger.warning("coco dataset type does not exist? Check database")
-        return
-
-    ### upload to s3 and make db entry
-    s3.upload_file_to_s3(checksum_fn, "dataset", checksum_fn)
-    s3.upload_zip_to_s3(dir_name, "dataset", zip_name=zip_name)
-    assert isinstance(coco_dataset_type.id, int)
-    writer.create_dataset(
-        datasetTypeId=coco_dataset_type.id,
-        name=dataset_name,
-        tags=tags,
-        links=links,
-        s3Key=f"dataset/{zip_name}",
-        checksumBlobS3Key=f"dataset/{checksum_fn}",
-    )
-
-    shutil.rmtree(dir_name)
+    upload_dataset_and_make_entry(dataset_name, tags, links, dir_name, checksum_blob, DatasetKeys.YOLO_FORMAT)
+    yolo_to_coco(dir_name, names)
+    upload_dataset_and_make_entry(dataset_name, tags, links, dir_name, checksum_blob, DatasetKeys.COCO_FORMAT)
 
     logger.info("Dataset conversions complete")
     sns.send(
         f"Converting {dtype} dataset",
         f"Converted dataset from {url}\ntimestamp: {time.strftime('%Y-%m-%d %H:%M:%S')}\ndatasets location: {GeneralKeys.S3_BUCKET_NAME}/datasets/",
     )
+    cleanup()
     return
 
 
@@ -462,6 +357,18 @@ def check_instance_terminated(instance_id):
     ec2 = boto3.client("ec2", region_name="us-east-1")
     response = ec2.describe_instance_status(InstanceIds=[instance_id], IncludeAllInstances=True)
     return response["InstanceStatuses"][0]["InstanceState"]["Name"] == "terminated"
+
+
+def cleanup():
+    keywords = ["yolo", "coco", "visdrone", "checksum", "old"]
+    for item in os.listdir("."):
+        if any(keyword in item for keyword in keywords):
+            print(item)
+            path = os.path.join(".", item)
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+            else:
+                os.remove(path)
 
 
 def listen_to_sqs():
